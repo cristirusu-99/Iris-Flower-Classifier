ceb18f029dbb5a413e4169399f78fbed69018c0b
eliza <letos_eliza16@yahoo.com>
2020-12-30 18:57:39 +0200

    final

---
+++ Authetication/IrisAuthD/Shared/LoginParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace IrisAuthD.Shared
+{
+    public class LoginParameters
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
