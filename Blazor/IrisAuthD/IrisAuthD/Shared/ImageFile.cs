d5fbebda00093e41531cc18b868d8685ee7845bf
cristirusu-99 <59122086+cristirusu-99@users.noreply.github.com>
2021-01-15 10:39:01 +0200

    Possible last working commit

---
+++ Blazor/IrisAuthD/IrisAuthD/Shared/ImageFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IrisAuthD.Shared
+{
+    public class ImageFile
+    {
+        public string base64data { get; set; }
+        public string fileName { get; set; }
+    }
+}
+
